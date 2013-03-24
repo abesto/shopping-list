a525bc1b677ca97143de83039deab8e53232da76
Zoltan Nagy <abesto@abesto.net>
2013-03-24 16:08:57 +0100

    Simplifications based on https://github.com/raimohanska/bacon.js/pull/125#issuecomment-15359806

--- routes/tab.js
+++ routes/tab.js
@@ -37,39 +37,39 @@
     var id = Bacon.fromNodeCallback(redis.incr, freeTabKey());
     var key = id.map(tabKey);
     var item = id.map(function (id) { return {text: req.body.text, id: id}; });
-    var create = Bacon.combineAsArray(key, item.map(JSON.stringify)).flatMapLatest(Bacon.redis('set'));
-    var addToList = id.flatMapLatest(Bacon.redis('rpush', tabsKey()));
+    var create = Bacon.combineAsArray(key, item.map(JSON.stringify)).flatMap(Bacon.redis('set'));
+    var addToList = id.flatMap(Bacon.redis('rpush', tabsKey()));
     Bacon.combineWith([item, create, addToList], fst).respond(res);
   };
 
   exports.update = function (req, res) {
-    var key = Bacon.once(req.params.id).map(tabKey).toProperty();
-    var json = key.flatMapLatest(Bacon.redis('get'));
+    var key = tabKey(req.params.id);
+    var json = Bacon.redis('get')(key);
     var obj = json.map(JSON.parse).map(function (o) { o.text = req.body.text; return o; });
-    var update = Bacon.combineAsArray(key, obj.map(JSON.stringify)).flatMapLatest(Bacon.redis('set'));
-    Bacon.combineWith([obj, update], fst).respond(res);
+    var update = obj.map(JSON.stringify).flatMap(Bacon.redis('set', key));
+    update.respond(res);
   };
 
   exports.list = function (req, res) {
     var ids = Bacon.fromNodeCallback(redis.lrange, tabsKey(), 0, -1);
     var keys = ids.filter(isEmpty(false)).map(_.partialRight(_.map, tabKey));
-    var jsons = keys.flatMapLatest(Bacon.redis('mget'));
+    var jsons = keys.flatMap(Bacon.redis('mget'));
     ids.filter(isEmpty(true)).respond(res);
     jsons.map(_.partialRight(_.map, JSON.parse)).respond(res);
   };
 
   exports.delete = function (req, res) {
     var tabId = Bacon.once(req.params.id).toProperty();
-    var _tabKey = tabId.map(tabKey).log();
+    var _tabKey = tabId.map(tabKey);
     var itemsOfTabKey = tabId.map(require('./item').itemsKey);
-    var itemIds = itemsOfTabKey.flatMapLatest(_.partialRight(Bacon.redis('lrange'), 0, -1));
+    var itemIds = itemsOfTabKey.flatMap(_.partialRight(Bacon.redis('lrange'), 0, -1));
     var noItems = itemIds.filter(isEmpty(true));
     // Delete items, if any
     var itemKeys = itemIds.filter(isEmpty(false)).map(_.partialRight(_.map, require('./item').itemKey));
-    var deleteItems = itemKeys.flatMapLatest(Bacon.redis('del'));
+    var deleteItems = itemKeys.flatMap(Bacon.redis('del'));
     // Delete the tab itself
-    var deleteTabFromTablist = tabId.flatMapLatest(Bacon.redis('lrem', tabsKey(), 1));
-    var deleteTab = _tabKey.flatMapLatest(Bacon.redis('del'));
+    var deleteTabFromTablist = tabId.flatMap(Bacon.redis('lrem', tabsKey(), 1));
+    var deleteTab = _tabKey.flatMap(Bacon.redis('del'));
     Bacon.combineWith([deleteItems.merge(noItems), deleteTabFromTablist, deleteTab], nop).respond(res);
   };
 }());
